a30f4db19cb4d5defb1fe830db85810b50f755e1
Moritz Bader <mo911bad@htwg-konstanz.de>
2026-01-09 12:50:17 +0100

    WebConfig aktualisiert

--- src/main/java/de/spotly/backend/config/WebConfig.java
+++ src/main/java/de/spotly/backend/config/WebConfig.java
@@ -13,11 +13,14 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") 
-                        .allowedOrigins("http://localhost:5173") 
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://backend-spotly.onrender.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(false);
+                        .allowCredentials(true);
             }
         };
     }
