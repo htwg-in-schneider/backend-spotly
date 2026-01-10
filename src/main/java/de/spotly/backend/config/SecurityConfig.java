0c8ef474d8d2ef0c975cebffeb6d4df95e740f82
Moritz Bader <mo911bad@htwg-konstanz.de>
2026-01-10 17:13:39 +0100

    Backend fixen

--- src/main/java/de/spotly/backend/config/SecurityConfig.java
+++ src/main/java/de/spotly/backend/config/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/reviews", "/api/reviews/**").permitAll()
 
                         .anyRequest().authenticated()
-                );
-               // .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
